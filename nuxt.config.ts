2abaf39d8c1b7bce5a478742aa6fbb0b52251c53
Артем Комаров <komarov.ar@office.lan>
2025-08-14 17:10:15 +0200

    feat: добавляет мета-тег для аккаунта Google AdSense

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,6 +31,10 @@ export default defineNuxtConfig({
             "Free online font converter tool. Convert TT, OTF to WOFF, WOFF2, and vice versa. Fast, secure, and easy to use font format conversion for web developers and designers.",
         },
         {
+          name: 'google-adsense-account',
+          content: 'ca-pub-8028101639830435'
+        },
+        {
           name: 'lang',
           content: 'en',
         },
